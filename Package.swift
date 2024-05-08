1d5535498c3aab60095ad7277b5fd16a55f01e86
Anka <runner@Mac-1715168279684.local>
2024-05-08 11:59:10 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,12 +9,27 @@ let package = Package(
     products: [
         .library(
             name: "TensorFlowLiteSwift",
-            targets: ["TensorFlowLiteSwift"]
+            targets: [
+                "TensorFlowLiteC",
+                "TensorFlowLiteCCoreML",
+                "TensorFlowLiteSwift"
+            ]
         )
     ],
     targets: [
         .target(
-            name: "TensorFlowLiteSwift"
+            name: "TensorFlowLiteSwift",
+            linkerSettings: [.linkedLibrary("c++")]
+        ),
+        .binaryTarget(
+            name: "TensorFlowLiteC",
+            url: "https://api.github.com/repos/tareksabry1337/TensorFlowLiteC/releases/assets/166724021.zip",
+            checksum: "a83c0845f034bf7477c895fb0a85394f492e70422008ed83fcc6a025d507d771"
+        ),
+        .binaryTarget(
+            name: "TensorFlowLiteCCoreML",
+            url: "https://api.github.com/repos/tareksabry1337/TensorFlowLiteC/releases/assets/166724019.zip",
+            checksum: "82a1952d5f736d0b903bc078db677a2e97aae5b2d21de5e8b62539bf8dbf8ea2"
         )
     ]
 )
